c6c22b2db1fb3c3dea0ae8aca626b6b60698a772
madumitha <smadumitha17@gmail.com>
2026-03-04 20:53:24 +0530

    Added Eureka Server, Config Server and API Gateway

---
+++ eureka-server/src/test/java/com/revconnect/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revconnect.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
