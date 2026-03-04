39b5c8bb9d24dcc486b067680347c514f2ed493c
madumitha <smadumitha17@gmail.com>
2026-03-05 00:52:06 +0530

    Added microservice modules and updated gitignore

---
+++ post-feed-service/src/main/java/com/revconnect/postfeedservice/PostFeedServiceApplication.java
@@ -0,0 +1,15 @@
+package com.revconnect.postfeedservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class PostFeedServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostFeedServiceApplication.class, args);
+    }
+
+}
---
+++ post-feed-service/src/test/java/com/revconnect/postfeedservice/PostFeedServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revconnect.postfeedservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostFeedServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
